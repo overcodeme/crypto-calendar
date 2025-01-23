3222d8832c2564f4b0c27366e5738c19e9b921b7
overcodeme <placmabadei1985@rambler.ru>
2025-01-24 02:22:43 +0300

    Add saving and reading events from json file

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import tkinter as tk
 from tkinter import messagebox, simpledialog
 from tkcalendar import Calendar
+import os
+import json
 
 
 class EventApp:
     def __init__(self, master):
         self.master = master
         self.master.title('Crypto Calendar')
+        self.master.geometry('800x600')
+
+        self.data_folder = 'data'
+        self.events_file = os.path.join(self.data_folder, 'events.json')
+        self.events = self.load_events()
 
         self.calendar = Calendar(master, selectmode='day', year=2025, month=1, day=1)
         self.calendar.pack(pady=20)
@@ -17,7 +24,17 @@ class EventApp:
         self.view_events_button = tk.Button(master, text='Просмотреть событие', command=self.view_events)
         self.view_events_button.pack(pady=10)
 
-        self.event = {}
+
+    def load_events(self):
+        if os.path.exists(self.events_file):
+            with open(self.events_file, 'r') as file:
+                return json.load(file)
+        return {}        
+
+
+    def save_events(self):
+        with open(self.events_file, 'w') as file:
+            json.dump(self.events, file)
 
     
     def add_event(self):
@@ -30,17 +47,14 @@ class EventApp:
             else:
                 self.events[date] = event
             messagebox.showinfo('Добавлено', 'Событие добавлено!')
+        self.save_events()
 
 
     def view_events(self):
         date = self.calendar.get_date()
         events_for_date = self.events.get(date, [])
-
-        if events_for_date:
-            events_text = '\n'.join(events_for_date)
-            messagebox.showinfo(f'События на {date}', events_text)
-        else:
-            messagebox.showinfo(f'События на {date}', 'Нет событий на этот день')
+        events_str = '\n'.join(events_for_date) if events_for_date else 'Нет событий'
+        messagebox.showinfo("События", events_str)
 
 
 if __name__ == '__main__':
